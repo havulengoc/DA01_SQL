6f9364dab49081ec8787c66c8e6fa7c997b44ac9
Ngoc Ha Vu <vulengocha@gmail.com>
2023-11-12 10:06:39 -0500

    Update practice1.sql
    Example 1 - 4

--- practice1.sql
+++ practice1.sql
@@ -1,1 +1,34 @@
+--- Example 1 (Hackerrank): Revising the Select query II
+/*
+Query the names of all American cities in CITY with populations larger than 120,000. The CountryCode for America is USA.
 
+## Input Format
+
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
+
+--- Example 2 (Hackerrank): Japanese Cities' Attributes
+SELECT *
+FROM City
+WHERE CountryCode = 'JPN';
+
+--- Example 3 (Hackerrank): Weather Observation Station 1
+SELECT CITY, STATE
+FROM STATION;
+
+--- Example 4 (Hackerrank): Weather Observation Station 6
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE "A%" OR CITY LIKE "E%" OR CITY LIKE "I%" OR CITY LIKE "O%" OR CITY LIKE "U%";
